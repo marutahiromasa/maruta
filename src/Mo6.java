32c5d8fef2eea383e556e8726b8adfb199168877
maruta hiromasa <marutahiromasa1@gmall.com>
2017-01-10 17:53:27 +0900

    next

--- src/Mo6.java
+++ src/Mo6.java
@@ -1,28 +1,41 @@
-
-
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Mo6 {
   public static void main(String[] args) {
-    if (args.length == 1) {
-      try {
-        FileReader in = new FileReader(args[0]);
-        int c;
-        while ((c = in.read()) != -1) {
-          System.out.print((char)c);
-        }
-        in.close();
-      } catch(IOException e) {
-        System.err.println(e);
-      }
-    } else {
-      System.err.println("usage: java cat1 filename");
-    }
-  }
-}
+	try{
 
+		int id; //ID
+		String name; //支店名
+		
+		Map<int, String> map = new HashMap<int, String>();
+		map.put(001, name);
+		int id = id++;
+		File file = new  File(args[0] + File.separator + "branch.list");
+		FileReader fr = new FileReader(file);
 
+		
+		
+
+		BufferedReader br = new BufferedReader(fr);
+		String s;
+		while((s = br.readLine()) != null) {
+			System.out.println(s + "を読み込みました");
+			
+			
+		}
+		br.close();
+
+		} catch(IOException e) {
+		  System.out.println("支店定義ファイルが存在しません");
+		  
+		
+		  
+		}
+	}
+ }
 
 
 
